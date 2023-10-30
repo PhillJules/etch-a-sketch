54808645ed2589a9c38380a9839043708d9ba605
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-30 21:47:45 +0000

    added function for one button
    after thinking about ways to give the user different sized grids, ways
    to make drawing on the etch-a-sketch more fun and interesting, I decided
    to make different divs for each button, and then have the buttons change
    the divs appearing on the screen. each one will have its own class which
    is activated by the button selected.

--- sketch.js
+++ sketch.js
@@ -1,24 +1,60 @@
-function setup() {
+// function setup() {
 let canvas = document.getElementById("canvas");
 
-  for (let i = 0; i < 4096; i++) {
-    let canvasBg = document.createElement('div')
-    canvasBg.className = "canvas-bg" // add class to div
-    // div.innerText = i // add text to div
-    canvas.appendChild(canvasBg) // add div to grid
+
+//   for (let i = 0; i < 4096; i++) {
+//     let canvasBg = document.createElement('div')
+//     canvasBg.className = "canvas-bg" // add class to div
+//     // div.innerText = i // add text to div
+//     canvas.appendChild(canvasBg) // add div to grid
+//   }
+
+// // add event listener to each grid item
+//   let canvasBg = document.getElementsByClassName("canvas-bg");
+//   for (let cell of canvasBg) {
+//     cell.addEventListener("mouseover", function(){
+//       cell.style.backgroundColor = "black"; // change background color
+//     });
+//   }
+
+
+// }
+
+
+
+// setup();
+
+
+function extraLarge() {
+  for (let i = 0; i < 256; i++){
+    let canvasXl = document.createElement('div')
+  canvasXl.className = "canvas-xl" // add class to div
+  canvas.appendChild(canvasXl) // add div to grid
   }
 
-// add event listener to each grid item
-  let canvasBg = document.getElementsByClassName("canvas-bg");
-  for (let cell of canvasBg) {
+  // add event listener to each grid item
+  let canvasXl = document.getElementsByClassName("canvas-xl");
+  for (let cell of canvasXl) {
     cell.addEventListener("mouseover", function(){
       cell.style.backgroundColor = "black"; // change background color
     });
   }
-
-
 }
 
+document.getElementById("xl").addEventListener("click", extraLarge);
 
 
-setup();
+// for (let i = 0; i < 256; i++) {
+//   let canvasBg = document.createElement('div')
+//   canvasBg.className = "canvas-bg" // add class to div
+//   canvas.appendChild(canvasBg) // add div to grid
+// }
+
+// // add event listener to each grid item
+// let canvasBg = document.getElementsByClassName("canvas-bg");
+//  for (let cell of canvasBg) {
+//    cell.addEventListener("mouseover", function(){
+//      cell.style.backgroundColor = "black"; // change background color
+//    });
+//  }
+// });
