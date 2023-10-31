3f402e936f73c4fbc886145ad22db36e119b9e15
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-31 20:10:15 +0000

    starting over
    i started noticing problems with the grid itself, it was too big and did'nt
    fit the vision i had for the project. i decided to start over and make a
    smaller grid. i also decided to make the grid a square instead of a rectangle.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 // function setup() {
-let canvas = document.getElementById("canvas");
+let grid = document.getElementById("grid");
 
 
 //   for (let i = 0; i < 4096; i++) {
@@ -25,40 +25,57 @@ let canvas = document.getElementById("canvas");
 // setup();
 
 
-function extraLarge() {
-  for (let i = 0; i < 256; i++){
-    let canvasXl = document.createElement('div')
-  canvasXl.className = "canvas-xl" // add class to div
-  canvas.appendChild(canvasXl) // add div to grid
-  }
+// function extraLarge() {
+//   for (let i = 0; i < 256; i++){
+//     let canvasXl = document.createElement('div')
+//   canvasXl.className = "canvas-xl" // add class to div
+//   canvas.appendChild(canvasXl) // add div to grid
+//   }
 
-  // add event listener to each grid item
-  let canvasXl = document.getElementsByClassName("canvas-xl");
-  for (let cell of canvasXl) {
-    cell.addEventListener("mouseover", function(){
-      cell.style.backgroundColor = "black"; // change background color
-    });
-  }
-}
+//   // add event listener to each grid item
+//   let canvasXl = document.getElementsByClassName("canvas-xl");
+//   for (let cell of canvasXl) {
+//     cell.addEventListener("mousedown", function(){
+//       cell.style.backgroundColor = "black"; // change background color
+//     });
+//   }
+// }
 
-function large() {
-  for (let i = 0; i < 1024; i++) {
-    let canvasLg = document.createElement('div')
-    canvasLg.className = "canvas-lg" // add class to div
-    canvas.appendChild(canvasLg) // add div to grid
-  }
+// function large() {
+//   for (let i = 0; i < 1024; i++) {
+//     let canvasLg = document.createElement('div')
+//     canvasLg.className = "canvas-lg" // add class to div
+//     canvas.appendChild(canvasLg) // add div to grid
+//   }
 
-  // add event listener to each grid item
-  let canvasLg = document.getElementsByClassName("canvas-lg");
-  for (let cell of canvasLg) {
-    cell.addEventListener("mouseover", function() {
-      cell.style.backgroundColor = "black"; // change background color
-    });
-  }
-}
+//   // add event listener to each grid item
+//   let canvasLg = document.getElementsByClassName("canvas-lg");
+//   for (let cell of canvasLg) {
+//     cell.addEventListener("mouseover", function() {
+//       cell.style.backgroundColor = "black"; // change background color
+//     });
+//   }
+// }
 
+// function medium() {
+//   for (let i = 0; i < 4096; i++){
+//     let canvasMd = document.createElement('div')
+//     canvasMd.className = "canvas-md" // add class to div
+//     canvas.appendChild(canvasMd) // add div to grid
+//   }
+
+//   // add event listener to each grid item
+//   let canvasMd = document.getElementsByClassName("canvas-md");
+//   for (let cell of canvasMd) {
+//     cell.addEventListener("mouseover", function(){
+//       cell.style.backgroundColor = "black"; // change background color
+//     });
+//   }
+// }
+
+
+document.getElementById("md").addEventListener("click", medium);
 document.getElementById("lg").addEventListener("click", large);
-
 document.getElementById("xl").addEventListener("click", extraLarge);
 
 
