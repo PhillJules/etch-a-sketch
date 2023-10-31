36c29a6b0efd3ed71cd8c8ed8ab3f65b009d3616
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-31 22:00:01 +0000

    the grid can now be drawn on
    so there is now a variable that is a boolean that is set to true when
    the mouse is pressed and false when it is released.
    
    this variable is used to determine whether or not to draw the grid.
    i need to now add an eraser effect to the grid. and a refresh button.
    from there i can add more complex things like a color picker and a rainbow effect.

--- sketch.js
+++ sketch.js
@@ -1,77 +1,6 @@
 // function setup() {
 let grid = document.getElementById("grid");
 
-
-//   for (let i = 0; i < 4096; i++) {
-//     let canvasBg = document.createElement('div')
-//     canvasBg.className = "canvas-bg" // add class to div
-//     // div.innerText = i // add text to div
-//     canvas.appendChild(canvasBg) // add div to grid
-//   }
-
-// // add event listener to each grid item
-//   let canvasBg = document.getElementsByClassName("canvas-bg");
-//   for (let cell of canvasBg) {
-//     cell.addEventListener("mouseover", function(){
-//       cell.style.backgroundColor = "black"; // change background color
-//     });
-//   }
-
-
-// }
-
-
-
-// setup();
-
-
-// function extraLarge() {
-//   for (let i = 0; i < 256; i++){
-//     let canvasXl = document.createElement('div')
-//   canvasXl.className = "canvas-xl" // add class to div
-//   canvas.appendChild(canvasXl) // add div to grid
-//   }
-
-//   // add event listener to each grid item
-//   let canvasXl = document.getElementsByClassName("canvas-xl");
-//   for (let cell of canvasXl) {
-//     cell.addEventListener("mousedown", function(){
-//       cell.style.backgroundColor = "black"; // change background color
-//     });
-//   }
-// }
-
-// function large() {
-//   for (let i = 0; i < 1024; i++) {
-//     let canvasLg = document.createElement('div')
-//     canvasLg.className = "canvas-lg" // add class to div
-//     canvas.appendChild(canvasLg) // add div to grid
-//   }
-
-//   // add event listener to each grid item
-//   let canvasLg = document.getElementsByClassName("canvas-lg");
-//   for (let cell of canvasLg) {
-//     cell.addEventListener("mouseover", function() {
-//       cell.style.backgroundColor = "black"; // change background color
-//     });
-//   }
-// }
-
-// function medium() {
-//   for (let i = 0; i < 4096; i++){
-//     let canvasMd = document.createElement('div')
-//     canvasMd.className = "canvas-md" // add class to div
-//     canvas.appendChild(canvasMd) // add div to grid
-//   }
-
-//   // add event listener to each grid item
-//   let canvasMd = document.getElementsByClassName("canvas-md");
-//   for (let cell of canvasMd) {
-//     cell.addEventListener("mouseover", function(){
-//       cell.style.backgroundColor = "black"; // change background color
-//     });
-//   }
-// }
 const sizeSelectors = document.querySelectorAll(".size");
 
 function gridSizeSettings(event) {
@@ -93,29 +22,24 @@ sizeSelectors.forEach((sizeSelector) => {
   sizeSelector.addEventListener("click", gridSizeSettings);
 })
 
+// add variable to keep track of whether or not drawing is happening
+let isDrawing = false;
 
-// function gridSize() {
-//   grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-//   grid.style.gridTemplateRows = "repeat(16, 1fr)";
-// }
+// add event listener to grid when drawing starts
+grid.addEventListener("mousedown",() =>{
+  isDrawing = true;
+});
+//add event listener to grid when drawing stops
+grid.addEventListener("mouseup",() =>{
+  isDrawing = false;
+});
 
-
-// document.getElementById("md").addEventListener("click", medium);
-// document.getElementById("lg").addEventListener("click", large);
-// document.getElementById("xl").addEventListener("click", extraLarge);
-
-
-// for (let i = 0; i < 256; i++) {
-//   let canvasBg = document.createElement('div')
-//   canvasBg.className = "canvas-bg" // add class to div
-//   canvas.appendChild(canvasBg) // add div to grid
-// }
-
-// // add event listener to each grid item
-// let canvasBg = document.getElementsByClassName("canvas-bg");
-//  for (let cell of canvasBg) {
-//    cell.addEventListener("mouseover", function(){
-//      cell.style.backgroundColor = "black"; // change background color
-//    });
-//  }
-// });
+// add event listener to grid when mouse moves on the grid
+grid.addEventListener("mousemove", (event) => {
+  if (isDrawing) {
+    const cell = event.target;
+    if (cell.classList.contains("grid-cell")) {
+      cell.style.backgroundColor = "black";
+    }
+  }
+});
